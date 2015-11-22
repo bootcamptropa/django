d4dfe7ecf8a520695b1b168bc2e8dc3e92c53aa9
Pepe-Padilla <jpadilla.isc@gmail.com>
2015-11-22 19:31:29 +0100

    UserDetail

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserDetail(models.Model):
+    user = models.OneToOneField(User, primary_key=True)
+    longitude = models.FloatField(null=True)
+    latitude = models.FloatField(null=True)
+    token_facebook = models.CharField(null=True)
+    avatar_url = models.URLField(null=True)
+
+
+    def __unicode__(self):
+        return self.name
