9dacdd114b4623b494b2da3f3f8902ef1ef2167c
Jose Enrique Sanchez Alfonso <jesanchez@jesanchez.es>
2015-12-31 16:54:22 +0100

    Aniadida paginacion a razas y estados

--- states/api.py
+++ states/api.py
@@ -1,19 +1,33 @@
 # -*- coding: utf-8 -*-
 
+from oauth2_provider.ext.rest_framework import OAuth2Authentication, TokenHasScope
+from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.pagination import PageNumberPagination
 from states.serializers import StatesSerializer
 from states.models import State
 
 class StatesViewSet (GenericViewSet):
 
-    pagination_class = PageNumberPagination
+    authentication_classes = [OAuth2Authentication]
+    permission_classes = [TokenHasScope]
+    required_scopes = ['read']
+
     serializer_class = StatesSerializer
     queryset = State.objects.all()
 
     def list(self, request):
 
-        states = State.objects.all()
-        self.paginate_queryset(states)  # pagino el resultado
-        serializer = StatesSerializer(states, many=True)
-        return self.get_paginated_response(serializer.data)  # devuelvo una respuesta paginada
+        queryset = self.filter_queryset(self.get_queryset())
+        page = self.paginate_queryset(queryset)
+
+        if page is not None:
+          serializer = self.get_serializer(page, many=True)
+          return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
+
+    def get_paginated_response(self, data):
+
+        assert self.paginator is not None
+        return self.paginator.get_paginated_response(data)
