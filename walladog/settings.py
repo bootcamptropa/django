9822a061e4c911235bd7da24f455c22d71d00324
Fernando Luna <337n4nd0@gmail.com>
2016-01-04 16:45:56 -0400

    uncomment prod connection data base

--- walladog/settings.py
+++ walladog/settings.py
@@ -97,14 +97,14 @@ DATABASES = {
         'HOST': 'mysql.develjitsu.com',
         'PORT': '3306',
     },
-    # 'prod': {
-    #     'ENGINE': 'django.db.backends.mysql',
-    #     'NAME': 'walladog',
-    #     'USER': 'walladog',
-    #     'PASSWORD': 'xxx',
-    #     'HOST': 'mysql.develjitsu.com',
-    #     'PORT': '3306',
-    # },
+    'prod': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'walladog',
+        'USER': 'walladog',
+        'PASSWORD': 'xxx',
+        'HOST': 'mysql.develjitsu.com',
+        'PORT': '3306',
+    },
 }
 
 # POR SI QUEREIS TRABAJAR EN LOCAL CON SQLITE
